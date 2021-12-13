faa8124220b8cf4e8a59eb93d11dc3280b47d0f7
ADITYA KUMAR <onlyaditya001@gmail.com>
2021-12-13 09:54:15 +0530

    working on home page

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+const STDIV = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  margin: 0px 10%;
+  //border: 1px solid red;
+  position: fixed;
+  top: 0;
+  color: white;
+  font-size: 1.2rem;
+
+  .block-1 {
+    display: flex;
+  }
+
+  .logo {
+    width: 50px;
+    border-radius: 50%;
+    margin-right: 1rem;
+  }
+
+  .title {
+    font-size: 1.5rem;
+    font-family: roboto;
+    font-weight: bold;
+  }
+
+  .links {
+    display: flex;
+  }
+
+  .link {
+    margin: 1rem;
+    cursor: pointer;
+  }
+`;
+
+export const Navbar = () => {
+  return (
+    <STDIV>
+      <div className="block-1">
+        <img className="logo" src="/Assets/LOGO.PNG" alt="" />
+        <img className="logo-text" src="/Assets/logo-text.png" alt="" />
+      </div>
+      <div className="links">
+        <div className="link">SignUp</div>
+        <div className="link">Login</div>
+        <div className="link">About</div>
+        <div className="link">Contact</div>
+      </div>
+    </STDIV>
+  );
+};
---
+++ src/Styles/Home.style.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const STDIV = styled.div`
+  background-image: url("https://dpbnri2zg3lc2.cloudfront.net/en/wp-content/uploads/2021/01/web_dev_pillar_page.jpg");
+  background-repeat: no-repeat;
+  background-size: 100%;
+  width: 98vw;
+  height: 100vh;
+  margin-top: 0;
+  border: 0.5px solid white;
+
+  .description {
+    width: 80%;
+    margin: 20% 10%;
+    color: white;
+    text-align: left;
+  }
+
+  .title-1 {
+    font-size: 2.5rem;
+  }
+
+  .title-2 {
+    font-size: 1.5rem;
+  }
+
+  .icons img {
+    width: 15%;
+    margin-top: 1.5rem;
+    margin-right: 1.5rem;
+    cursor: pointer;
+  }
+`;
