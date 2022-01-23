92071b99a38f3ec977a4a4eed911f902664c96fa
kamal <kamaldro97@gmail.com>
2022-01-23 20:40:13 +0530

    some bug fixed in chat

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import App from "./App";
 import socketIOClient from "socket.io-client";
 import SocketContext from "./Context/SocketContext";
 import { CookiesProvider } from "react-cookie";
+import { Provider } from "react-redux";
 const ENDPOINT = "http://localhost:5000";
 const socket = socketIOClient(ENDPOINT);
 
 ReactDOM.render(
   <React.StrictMode>
-    <CookiesProvider>
-      <SocketContext.Provider value={socket}>
-        <App />
-      </SocketContext.Provider>
-    </CookiesProvider>
+    <Provider store={store}>
+      <CookiesProvider>
+        <SocketContext.Provider value={socket}>
+          <App />
+        </SocketContext.Provider>
+      </CookiesProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
