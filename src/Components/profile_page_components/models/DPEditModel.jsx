98ec1529e78b186c8a402682bb2b82db099695e8
rahul rajput <rahulrajput7358@gmail.com>
2022-01-25 22:54:03 +0530

    avatar done

--- src/Components/profile_page_components/models/DPEditModel.jsx
+++ src/Components/profile_page_components/models/DPEditModel.jsx
@@ -1,13 +1,11 @@
-import ClearIcon from "@mui/icons-material/Clear";
+
 import styled from "styled-components";
-import { Dialog } from "@mui/material";
-import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
-import CameraAltOutlinedIcon from "@mui/icons-material/CameraAltOutlined";
-import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { updateDP } from "../../Redux/action";
+import { updateDP } from "../../../Redux/action";
 import AvatarEditor from "react-avatar-editor";
+import { Model } from './Model.material';
 
 const DPEditModel = ({ toggleModel }) => {
   // for showing input ranges for zoom level and straight level
@@ -40,13 +38,13 @@ const DPEditModel = ({ toggleModel }) => {
   const uploadImg = async () => {
     if (editedImg) {
       dispatch(updateDP(editedImg.getImageScaledToCanvas().toDataURL()));
-      toggleModel();
+     
     } else if (imgData.imagePreviewUrl) {
       dispatch(updateDP(imgData.imagePreviewUrl));
-      toggleModel();
+      
     }
 
-    /// it is under construction right now
+    /// --------------------------------it is under construction right now
 
     // let file = imgData.file;
     // const form = new FormData();
@@ -69,15 +67,10 @@ const DPEditModel = ({ toggleModel }) => {
   };
   
   return (
-    <StyledDialog open={true} onClose={() => toggleModel()}>
-      <StyledDialogTitle>
-        <h3> Profile Photo </h3>
-        <StyledClearIcon onClick={() => toggleModel()} />
-      </StyledDialogTitle>
-      <StyledDialogContent>
-          {showEditCont ?
+    <Model modelName={"Profile Photo"} onSave={uploadImg} toggleModel={toggleModel} isImgModel={{onEdit:() => setShowEditCont(!showEditCont), onImgInputChange: (e) => handleImageChange(e) }}>
+        {showEditCont ?
           <>
-            <div style={{"width":"60%", "margin" :"auto"}}>
+            <div style={{"width":"70%", "margin" :"auto"}}>
                 <StyledAvatarEditor
                   ref={(editor) => setEditedImg(editor)}
                   image={imgData.imagePreviewUrl || dp}
@@ -87,7 +80,7 @@ const DPEditModel = ({ toggleModel }) => {
                   borderRadius={115}
                   rotate={+imgRotateLevel}
                 />
-              </div>
+            </div>
               <StyledEditingMode>
                 <div className="inputRange">
                   <label htmlFor="">
@@ -115,35 +108,8 @@ const DPEditModel = ({ toggleModel }) => {
             </> : <StyledImagePreview>
               <img src={imgData.imagePreviewUrl || dp} alt="" />
             </StyledImagePreview>
-          }
-       
-      </StyledDialogContent>
-      <footer>
-        <StyledIconsCont>
-          <div onClick={() => setShowEditCont(!showEditCont)}>
-            <ModeEditOutlineOutlinedIcon />
-            <h4>Edit</h4>
-          </div>
-          <div>
-            <label htmlFor="fileInput">
-              <CameraAltOutlinedIcon />
-              <h4>Add photo</h4>
-              <input
-                id="fileInput"
-                type="file"
-                style={{ display: "none" }}
-                onChange={(e) => handleImageChange(e)}
-              />
-            </label>
-          </div>
-          <div>
-            <DeleteOutlineOutlinedIcon />
-            <h4>Delete</h4>
-          </div>
-        </StyledIconsCont>
-        <button onClick={uploadImg}>Save</button>
-      </footer>
-    </StyledDialog>
+      }
+    </Model>
   );
 };
 
@@ -151,8 +117,10 @@ export { DPEditModel };
 const StyledImagePreview = styled.div`
   width:280px;
   height:280px;
-  border-radius: 50%;
   margin:auto;
+  margin-top:20px;
+  margin-bottom:20px;
+  border-radius: 50%;
   & > img {
     width:100%;
     height:100%;
@@ -180,67 +148,4 @@ const StyledEditingMode = styled.div`
 const StyledAvatarEditor = styled(AvatarEditor)`
   width: 100% !important;
   height: 100% !important;
-`;
-const StyledDialog = styled(Dialog)`
-  &
-    div.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation24.MuiDialog-paper.MuiDialog-paperScrollPaper {
-    min-width: 40%;
-    background-color: white;
-  }
-`;
-const StyledDialogTitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 5% 0 5%;
-`;
-const StyledDialogContent = styled.div`
-  padding: 2% 5% 2% 5%;
-  border: 1px solid gainsboro;
-  border-left: none;
-  border-right: none;
-  & > input {
-    width: 100%;
-    padding: 10px;
-    font-size: 15px;
-    margin-top: 20px;
-    box-sizing: border-box;
-  }
-  & + footer {
-    display: flex;
-    justify-content: space-between;
-    padding: 2% 5% 2% 5%;
-    button {
-      padding: 10px 20px 10px 20px;
-      background-color: rgb(0, 200, 5);
-      color: white;
-      cursor: pointer;
-      font-size: 15px;
-      border: none;
-      border-radius: 30px;
-    }
-  }
-`;
-const StyledIconsCont = styled.div`
-  width: 45%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  & > div {
-    cursor: pointer;
-    text-align: center;
-    h4 {
-      margin: 0;
-    }
-    label {
-      cursor: inherit;
-    }
-  }
-  & > div:hover {
-    color: rgb(0, 200, 5);
-  }
-`;
-
-const StyledClearIcon = styled(ClearIcon)`
-  cursor: pointer;
 `;
